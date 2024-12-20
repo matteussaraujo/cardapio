3cbb90310773c80af25940b3f37f0df8d013b692
Mateus <mateusdev36@gmail.com>
2024-12-19 21:04:31 -0300

    Adicionadas as funcionalidades, listar produtos, adicionar produtos ao carrinho, verificar itens no carrinho pela home, acessar detalhes do carrinho, remover item do carrinho entre outras

---
+++ script.js
@@ -0,0 +1,128 @@
+const menu = document.getElementById("menu")
+const cartBtn = document.getElementById("cart-btn")
+const cartModal = document.getElementById("cart-modal")
+const cartItemsContainer = document.getElementById("cart-items")
+const cartTotal = document.getElementById("cart-total")
+const checkoutBtn = document.getElementById("checkout-btn")
+const closeModalBtn = document.getElementById("close-modal-btn")
+const cartCounter = document.getElementById("cart-count")
+const addressInput = document.getElementById("address")
+const addressWarn = document.getElementById("address-warn")
+
+
+let cart = [];
+
+
+cartBtn.addEventListener("click", function(){
+    cartModal.style.display = "flex"
+    updateCartModal();
+})
+
+cartModal.addEventListener("click", function(event){
+    if(event.target === cartModal){
+        cartModal.style.display = "none"
+    }
+})
+
+closeModalBtn.addEventListener("click", function(){
+    cartModal.style.display = "none"
+})
+
+menu.addEventListener("click", function(event){
+    let parentButton = event.target.closest(".add-to-cart-btn")
+    
+    if(parentButton){
+
+        const name = parentButton.getAttribute("data-name")
+        const price = parseFloat(parentButton.getAttribute("data-price"))
+        
+        addToCart(name, price)
+    }
+})
+
+function addToCart(name, price){
+
+    const existingItem = cart.find(item => item.name === name)
+
+    if(existingItem){
+        existingItem.quantity += 1;
+        
+    } else{
+        cart.push({
+            name,
+            price,
+            quantity:1,
+        })
+    }
+
+   
+
+    updateCartModal()
+}
+
+
+function updateCartModal(){
+    cartItemsContainer.innerHTML = "";
+    let total = 0;
+
+    cart.forEach(item => {
+        const cartItemElement = document.createElement("div");
+        cartItemElement.classList.add("flex", "justify-between", "mb-4", "flex-col")
+        
+        cartItemElement.innerHTML = `
+        <div class="flex items-center justify-between">
+        <div>
+        <p class="font-bold">${item.name}</p>
+        <p>Qtd: ${item.quantity}</p>
+        <p class="font-medium mt-2">R$ ${item.price.toFixed(2)}</p>
+        </div>
+
+        
+        <button class="remove-from-cart-btn" data-name="${item.name}">
+        Remover
+        </button>
+
+    
+        </div>
+    
+        `
+        total += item.price * item.quantity;
+
+        cartItemsContainer.appendChild(cartItemElement)
+    })
+        
+    cartTotal.textContent = total.toLocaleString("PT-BR", {
+        style:"currency",
+        currency: "BRL"
+    });
+
+    const totalItems = cart.reduce((sum, item) => sum + item.quantity, 0);
+    cartCounter.innerHTML = totalItems;
+    
+}
+
+cartItemsContainer.addEventListener("click", function(event){
+    if(event.target.classList.contains("remove-from-cart-btn")){
+        const name = event.target.getAttribute("data-name")
+        removeItemCart(name);
+    }
+})
+
+function removeItemCart (name){
+    const index = cart.findIndex(item => item.name === name);
+
+    if(index !== -1){
+        const item = cart[index];
+
+        if(item.quantity > 1){
+            item.quantity -= 1;
+            updateCartModal();
+            return;
+        }
+    }
+    
+   
+
+    cart.splice(index, 1)
+    updateCartModal();
+}
